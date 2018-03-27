a1066b9ac2294a3643ed7b0f82491edabd138dc7
Justson <xiaozhongcen@gmail.com>
2018-03-27 23:45:53 +0800

    #

--- app/model/authorization.js
+++ app/model/authorization.js
@@ -16,9 +16,6 @@ module.exports = app => {
         created_at: DATE,
         updated_at: DATE,
     });
-    // Authorization.prototype.associate = function () {
-    //     app.model.Authorization.hasMany(app.model.Post, {as: 'posts', foreignKey: 'user_id'});
-    // };
 
     return Authorization;
 };
